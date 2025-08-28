1e24672f95f463afaeef36a4343126eff7352b6d
Nuon-Hokseng <nuon.hokseng24@kit.edu.kh>
2025-08-28 15:19:50 +0700

    work now

--- src/app/confirm/page.tsx
+++ src/app/confirm/page.tsx
@@ -1,7 +1,7 @@
 import { createClient } from "../../supabase/server";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export async function handler(
+export default async function ConfirmEmailPage(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
